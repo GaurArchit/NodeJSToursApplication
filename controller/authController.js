dfad505f3f8ad7e3785d16203ed7898b52e82d3a
Archit Gaur <architgaur@Archits-MacBook-Pro.local>
2026-09-05 22:43:52 +0530

    Sept 5 code of token and setting up of deletion of token by role admin

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { promisify } = require('util');;
+const { promisify } = require('util');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
@@ -10,7 +10,7 @@ exports.signup = catchAsync(async (req, res) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAT: req.body.passwordChangedAT
+    role: req.body.role,
   });
   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -49,7 +49,6 @@ exports.login = catchAsync(async (req, res, next) => {
   });
 });
 
-
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
   let token;
@@ -62,23 +61,34 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   if (!token) {
     return next(
-      new AppError('You are not logged in! Please log in to get access.', 401)
+      new AppError('You are not logged in! Please log in to get access.', 401),
     );
   }
-
-  // 2) Verification token
+// 2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
   console.log(decoded);
-//3. check if the user still exits 
-const frshUser=await User.findById(decoded.id);
-if(!frshUser){
-  return next(new AppError('User no longer exist' ,401));
-}
-// 4. check if user change password after JWT issued
- if(frshUser.changedPasswordAfter(decoded.iat)) {
-  return next(new AppError('User changed passpwrd please log in again ',401));
- };
-
- //Grant access to the protected route 
+  //3. check if the user still exits
+  const frshUser = await User.findById(decoded.id);
+  if (!frshUser) {
+    return next(new AppError('User no longer exist', 401));
+  }
+  // 4. check if user change password after JWT issued
+  if (frshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User changed passpwrd please log in again ', 401),
+    );
+  }
+  //Grant access to the protected route
+  req.user=frshUser; //By this we are sending the fresh user value to the entire middle ware 
   next();
-})
+});
+
+exports.restrictTo=(...roles)=>{
+  return(req,res,next)=>{
+    //roles [admin,lead-guide]
+    if(!roles.includes(req.user.role)){
+      return next( new AppError("Only admin or lead-guide can delete",403))
+    }
+    next();
+  }
+}
--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -29,6 +29,6 @@ router
   .get(tourController.getTour)
   .post(tourController.updateTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(authController.protect,authController.restrictTo('admin','lead-guide'),tourController.deleteTour);
 
 module.exports = router;
